8626a774be8d48c32227b316844e9dfe1e62ac65
eg2466 <73257930+eg2466@users.noreply.github.com>
2020-12-10 03:39:47 +0300

    updated file location

--- public/main.js
+++ public/main.js
@@ -60,34 +60,34 @@ function resetGame () {
 }
 
 function preload(){
-    bg = loadImage('../assets/bkg.png');
+    bg = loadImage('./assets/bkg.png');
     
-    tile_img = loadImage('../assets/Tile.png');
+    tile_img = loadImage('./assets/Tile.png');
     
-    fb_img = loadImage('../assets/SoccerBall.png');
+    fb_img = loadImage('./assets/SoccerBall.png');
     
-    leftnet_left_pole_img = loadImage('../assets/net/left_pole.png');
-    leftnet_bottom_pole_img = loadImage('../assets/net/bottom_pole.png');
-    leftnet_right_pole_img = loadImage('../assets/net/right_pole.png');
-    leftnet_top_pole_img = loadImage('../assets/net/top_pole.png');
-    leftnet_net_img = loadImage('../assets/net/net.png');
+    leftnet_left_pole_img = loadImage('./assets/net/left_pole.png');
+    leftnet_bottom_pole_img = loadImage('./assets/net/bottom_pole.png');
+    leftnet_right_pole_img = loadImage('./assets/net/right_pole.png');
+    leftnet_top_pole_img = loadImage('./assets/net/top_pole.png');
+    leftnet_net_img = loadImage('./assets/net/net.png');
     
-    rightnet_right_pole_img = loadImage('../assets/net/right/r_right_pole.png');
-    rightnet_bottom_pole_img = loadImage('../assets/net/right/r_bottom_pole.png');
-    rightnet_left_pole_img = loadImage('../assets/net/right/r_left_pole.png');
-    rightnet_top_pole_img = loadImage('../assets/net/right/r_top_pole.png');
-    rightnet_net_img = loadImage('../assets/net/right/right_net.png');
+    rightnet_right_pole_img = loadImage('./assets/net/right/r_right_pole.png');
+    rightnet_bottom_pole_img = loadImage('./assets/net/right/r_bottom_pole.png');
+    rightnet_left_pole_img = loadImage('./assets/net/right/r_left_pole.png');
+    rightnet_top_pole_img = loadImage('./assets/net/right/r_top_pole.png');
+    rightnet_net_img = loadImage('./assets/net/right/right_net.png');
  
-    bonus_1_img = loadImage('../assets/bonus_1.png');
-    bonus_2_img = loadImage('../assets/bonus_2.png');
+    bonus_1_img = loadImage('./assets/bonus_1.png');
+    bonus_2_img = loadImage('./assets/bonus_2.png');
     
-    bonus_img = loadAnimation('../assets/bonus_1.png', '../assets/bonus_2.png');
+    bonus_img = loadAnimation('./assets/bonus_1.png', './assets/bonus_2.png');
     
-    kick_sprite_sheet = loadSpriteSheet('../assets/kicksprite.png', kick_frames);
+    kick_sprite_sheet = loadSpriteSheet('./assets/kicksprite.png', kick_frames);
     
     player_kick = loadAnimation(kick_sprite_sheet);
      
-    player_stand = loadAnimation(new SpriteSheet('../assets/kicksprite.png',
+    player_stand = loadAnimation(new SpriteSheet('./assets/kicksprite.png',
     [{'name':'player_stand', 'frame':{'x':0, 'y': 0, 'width': 65, 'height': 76}} ]));
     
 }
